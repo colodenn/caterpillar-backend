9d3841304aabb8dcb77ffb5bfc624f41c39bcc63
Cornelius Denninger <s8codenn@stud.uni-saarland.de>
2021-03-07 13:31:28 +0100

    :chart_with_upwards_trend: Added petrinet and docstrings
    petrinet and docstrings

--- app.py
+++ app.py
@@ -1,27 +1,20 @@
-import imghdr
+# imports
+import os
 from flask_cors import CORS
-from pm4py.objects.log.importer.xes import importer as xes_importer
+from pycode.statistics.statistics import *
 import pandas as pd
 import numpy as np
-from pm4py.statistics.traces.log import case_statistics
-from pm4py.statistics.traces.log import case_statistics
-from pm4py.objects.log.util import interval_lifecycle
-
-import os
-from flask import Flask, render_template, request, redirect, url_for, abort, \
-    send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, abort, send_from_directory, send_file
 from werkzeug.utils import secure_filename
 from flask import jsonify
+
+# config
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 500 * 1024 * 1024
 app.config['UPLOAD_EXTENSIONS'] = ['.csv','.xes']
 app.config['UPLOAD_PATH'] = 'Uploads'
 CORS(app)
 
-
-@app.errorhandler(413)
-def too_large(e):
-    return "File is too large", 413
 
 @app.route('/')
 def index():
@@ -38,101 +31,39 @@ def upload_files():
             return 400
         
     uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
-    from pm4py.objects.log.importer.xes import importer as xes_importer
-    variant = xes_importer.Variants.ITERPARSE
-    parameters = {variant.value.Parameters.TIMESTAMP_SORT: True}
-    log = xes_importer.apply(os.path.join(app.config['UPLOAD_PATH'], filename),
-                            variant=variant, parameters=parameters)
-    def createDataFrame(log):
-        newlog = []
-        caseid = 0
-        df = []
-        for l in log:
-            caseid += 1
-            for i in l:
-                i['caseid'] = caseid
-                newlog = newlog + [i] 
-        df = pd.DataFrame(newlog)
-        return df
-
+   
+    log = createLog(app.config['UPLOAD_PATH'],filename)
+    
     df = createDataFrame(log)
 
     activitiesCount = getActivitesCount(df).to_dict()
     activitiesCount = dictToArray(activitiesCount)
-    print(type(activitiesCount))
     eventCount = getEventCount(df)
-    print(type(eventCount))
-
     meanDurchlaufzeit = getMeanDurchlaufzeit(log)
-    print(type(meanDurchlaufzeit))
-
     resourceCount = getResourceCount(df).to_dict()
     resourceCount = dictToArray(resourceCount)
-
-
     durchlaufzeit = getDurchlaufzeit(log)
-    print(type(durchlaufzeit))
-
-
-
+    print(getPetrinet(log,'Uploads/petrinet/%s.png' % filename))
     return {
      "activitiesCount": activitiesCount,
      "eventCount": eventCount, 
      "meanDurchlaufzeit": meanDurchlaufzeit,
      "resourceCount": resourceCount,
-     "durchlaufzeit" : durchlaufzeit
+     "durchlaufzeit" : durchlaufzeit,
+     "image": os.path.join(app.config['UPLOAD_PATH'],'petrinet','%s.png' % filename)
       }
+
 
 @app.route('/uploads/<filename>')
 def upload(filename):
     return send_from_directory(app.config['UPLOAD_PATH'], filename)
 
+@app.route('/uploads/petrinet/<filename>')
+def petrinet(filename):
+    string = 'Uploads/petrinet/%s' % filename
+    print(string)
+    return send_file(string, mimetype='image/svg')
 
-def dictToArray(dic):
-    arr = []
-    for key in dic:
-        arr.append({"name": key, "count": dic[key]})
-
-    return arr
-
-def getUniqueActivities(df):
-    return (len(df["concept:name"].unique()),df["concept:name"].unique())
-
-def getActivitesCount(df):
-    return df["concept:name"].value_counts()
-
-def getEventCount(df):
-    return len(df)
-
-def getStartEnd(df):
-    start = df.iloc[0]['time:timestamp']
-    end = df.iloc[-1]['time:timestamp']
-    return (start,end)
-
-def getEventCount(df):
-    return (len(df['caseid']))
-
-def getDurchlaufzeit(log):
-    all_case_durations = case_statistics.get_all_casedurations(log, parameters={
-    case_statistics.Parameters.TIMESTAMP_KEY: "time:timestamp"})
-    return all_case_durations
-
-def getMeanDurchlaufzeit(log):
-    median_case_duration = case_statistics.get_median_caseduration(log, parameters={
-        case_statistics.Parameters.TIMESTAMP_KEY: "time:timestamp"
-    })
-    
-    return median_case_duration
-              
-def getWaitingtime(log):
-    enriched_log = interval_lifecycle.assign_lead_cycle_time(log)
-    return enriched_log
-
-def getResourceCount(df):
-    return (df['org:resource'].value_counts())
-
-def getUniqueResource(df):
-    return (len(df["org:resource"].unique()),df["org:resource"].unique())
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- pycode/statistics/statistics.py
+++ pycode/statistics/statistics.py
@@ -1,11 +1,13 @@
 from pm4py.objects.log.importer.xes import importer as xes_importer
 import pandas as pd
 import numpy as np
+import os
 from pm4py.statistics.traces.log import case_statistics
 from pm4py.statistics.traces.log import case_statistics
 from pm4py.objects.log.util import interval_lifecycle
 from pm4py.objects.log.importer.xes import importer as xes_importer
-
+from pm4py.algo.discovery.heuristics import algorithm as heuristics_miner
+from pm4py.visualization.petrinet import visualizer as pn_visualizer
 def dictToArray(dic):
     arr = []
     for key in dic:
@@ -20,29 +22,76 @@ def getActivitesCount(df):
     return df["concept:name"].value_counts()
 
 def getEventCount(df):
+    """Get event count of dataframe
+
+    Args:
+        df (Dataframe): Pandas dataframe
+
+    Returns:
+        Integer: Number of Events
+    """
     return len(df)
 
 def getStartEnd(df):
+    """Get Start and End timestamp from dataframe
+
+    Args:
+        df (Dataframe): Pandas dataframe
+
+    Returns:
+        Tuple: (Start timestamp, End timestamp)
+    """
     start = df.iloc[0]['time:timestamp']
     end = df.iloc[-1]['time:timestamp']
     return (start,end)
 
-def getEventCount(df):
+def getCaseCount(df):
+    """Get case count
+
+    Args:
+        df (Dataframe): Pandas dataframe
+
+    Returns:
+        Integer: case count 
+    """
     return (len(df['caseid']))
 
 def getDurchlaufzeit(log):
+    """Get Throughputtime
+
+    Args:
+        log (Eventlog): PM4PY Eventlog
+
+    Returns:
+        [list]: Returns throughputtime per case
+    """
     all_case_durations = case_statistics.get_all_casedurations(log, parameters={
     case_statistics.Parameters.TIMESTAMP_KEY: "time:timestamp"})
     return all_case_durations
 
 def getMeanDurchlaufzeit(log):
+    """Mean throughputtime
+
+    Args:
+        log (Eventlog): PM4PY Eventlog
+
+    Returns:
+        Float: Mean throughputtime
+    """
     median_case_duration = case_statistics.get_median_caseduration(log, parameters={
         case_statistics.Parameters.TIMESTAMP_KEY: "time:timestamp"
     })
-    
     return median_case_duration
-              
+      
 def getWaitingtime(log):
+    """Get Waitingtime
+
+    Args:
+        log (EventLog): PM4PY Eventlog
+
+    Returns:
+        Eventlog: Returns enrichedlog with additional information about waiting time
+    """
     enriched_log = interval_lifecycle.assign_lead_cycle_time(log)
     return enriched_log
 
@@ -64,11 +113,12 @@ def createDataFrame(log):
         df = pd.DataFrame(newlog)
         return df
 
-def createLog(path):
+def createLog(path,filename):
     variant = xes_importer.Variants.ITERPARSE
     parameters = {variant.value.Parameters.TIMESTAMP_SORT: True}
     log = xes_importer.apply(os.path.join(path, filename),
                             variant=variant, parameters=parameters)
+    return log
 
 def dictToArray(dic):
     arr = []
@@ -76,3 +126,10 @@ def dictToArray(dic):
         arr.append({"name": key, "count": dic[key]})
 
     return arr
+
+def getPetrinet(log,path):
+    net, im, fm = heuristics_miner.apply(log, parameters={heuristics_miner.Variants.CLASSIC.value.Parameters.DEPENDENCY_THRESH: 0.99})
+    gviz = pn_visualizer.apply(net, im, fm)
+    image = pn_visualizer.save(gviz,path)
+
+    return image
